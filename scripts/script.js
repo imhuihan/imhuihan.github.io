b0dfbafd0399135b4d60740461c8f489dc19f180
imhuihan <zhanghuihan4869@gmail.com>
2024-02-15 13:41:56 -0500

    .
    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-document.getElementById("toggleButton").addEventListener("click", function() {
+document.getElementById("p1").addEventListener("click", function() {
     var paragraph = document.getElementById("paragraph");
     if (paragraph.style.display === "none") {
       paragraph.style.display = "block";
@@ -6,3 +6,6 @@ document.getElementById("toggleButton").addEventListener("click", function() {
       paragraph.style.display = "none";
     }
   });
+
+
+  
