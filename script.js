fff19c785ee0a6a72ca42cab8399a571ea4988de
imhuihan <zhanghuihan4869@gmail.com>
2024-02-15 14:02:48 -0500

    .
    .

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ document.getElementById("p1").addEventListener("click", function() {
   });
 
 
+  document.getElementById("p2").addEventListener("click", function() {
+    var paragraph = document.getElementById("paragraph2");
+    if (paragraph.style.display === "none") {
+      paragraph.style.display = "block";
+    } else {
+      paragraph.style.display = "none";
+    }
+  });
+
+
   
