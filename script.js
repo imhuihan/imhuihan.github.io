35683a8e23dc94a1918215c0b4fbfb27dcc145eb
imhuihan <zhanghuihan4869@gmail.com>
2024-02-15 13:56:34 -0500

    .
    .

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("p1").addEventListener("click", function() {
+    var paragraph = document.getElementById("paragraph");
+    if (paragraph.style.display === "none") {
+      paragraph.style.display = "block";
+    } else {
+      paragraph.style.display = "none";
+    }
+  });
+
+
+  
