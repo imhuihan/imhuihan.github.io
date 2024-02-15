6386f96712a96e7b9756f8e885791ed7053f141a
imhuihan <zhanghuihan4869@gmail.com>
2024-02-15 13:07:39 -0500

    added abstract button

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+document.getElementById("toggleButton").addEventListener("click", function() {
+    var paragraph = document.getElementById("paragraph");
+    if (paragraph.style.display === "none") {
+      paragraph.style.display = "block";
+    } else {
+      paragraph.style.display = "none";
+    }
+  });
