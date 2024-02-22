160d6ea3a1b9f1c1810643c6e1b2fdafa06a62ee
imhuihan <zhanghuihan4869@gmail.com>
2024-02-22 10:00:01 -0500

    .
    .

--- script.js
+++ script.js
@@ -18,4 +18,30 @@ document.getElementById("p1").addEventListener("click", function() {
   });
 
 
+  document.getElementById("p3").addEventListener("click", function() {
+    var paragraph = document.getElementById("paragraph3");
+    if (paragraph.style.display === "none") {
+      paragraph.style.display = "block";
+    } else {
+      paragraph.style.display = "none";
+    }
+  });
+
+
+  
+
+
+  document.getElementById("p4").addEventListener("click", function() {
+    var paragraph = document.getElementById("paragraph4");
+    if (paragraph.style.display === "none") {
+      paragraph.style.display = "block";
+    } else {
+      paragraph.style.display = "none";
+    }
+  });
+
+
+  
+
+  
   
